c1288e00b68613160b0d570ef72850bd85ffca74
Hector Machin <hector@element84.com>
2024-05-21 12:28:10 -0400

    Formatting fixes

--- profiles/cirrus/inputs.tf
+++ profiles/cirrus/inputs.tf
@@ -19,27 +19,27 @@ variable "environment" {
 variable "cirrus_inputs" {
   description = "Inputs for Cirrus FilmDrop deployment."
   type = object({
-    data_bucket     = string
-    payload_bucket  = string
+    data_bucket    = string
+    payload_bucket = string
     process = object({
       sqs_timeout           = number
       sqs_max_receive_count = number
     })
     state = object({
-      timestream_magnetic_store_retention_period_in_days  = number
-      timestream_memory_store_retention_period_in_hours   = number
+      timestream_magnetic_store_retention_period_in_days = number
+      timestream_memory_store_retention_period_in_hours  = number
     })
   })
   default = {
-    data_bucket     = "cirrus-data-bucket-name"
-    payload_bucket  = "cirrus-payload-bucket-name"
+    data_bucket    = "cirrus-data-bucket-name"
+    payload_bucket = "cirrus-payload-bucket-name"
     process = {
       sqs_timeout           = 180
       sqs_max_receive_count = 5
     }
     state = {
-      timestream_magnetic_store_retention_period_in_days  = 93
-      timestream_memory_store_retention_period_in_hours   = 24
+      timestream_magnetic_store_retention_period_in_days = 93
+      timestream_memory_store_retention_period_in_hours  = 24
     }
   }
 }
