88b5c4df40b2cd92a09c0bf05d5937f8cc131e58
bick4ord <bick4ord@users.noreply.github.com>
2024-05-24 08:44:08 -0400

    Updates to match a previous change to private/public subnets_az_to_id_map (#54)
    * Updates to match a previous change to private and public subnets_az_to_id_map, which was previously mapping subnet_az to cidr_block.  This has the side effect of no longer creating the VPC and subnets in the VPC module.  The module consumer must now create the VPC and subnets prior to using the FD vpc module, so that IDs can be provided.

--- modules/base_infra/vpc_infra/vpc_endpoints.tf
+++ modules/base_infra/vpc_infra/vpc_endpoints.tf
@@ -3,7 +3,7 @@ module "gateway_endpoints" {
 
   for_each = toset(var.gateway_endpoints_list)
 
-  vpc_id          = aws_vpc.filmdrop_vpc.id
+  vpc_id          = data.aws_vpc.filmdrop_vpc.id
   service_name    = "com.amazonaws.${data.aws_region.current.name}.${each.value}"
   route_table_ids = concat([aws_route_table.public_route_table.id], values(aws_route_table.private_route_tables)[*].id)
 }
@@ -13,9 +13,9 @@ module "interface_endpoints" {
 
   for_each = toset(var.interface_endpoints_list)
 
-  vpc_id              = aws_vpc.filmdrop_vpc.id
+  vpc_id              = data.aws_vpc.filmdrop_vpc.id
   service_name        = "com.amazonaws.${data.aws_region.current.name}.${each.value}"
   security_group_ids  = [aws_security_group.filmdrop_vpc_default_sg.id]
-  subnet_ids          = values(aws_subnet.private_subnets)[*].id
+  subnet_ids          = values(data.aws_subnet.private_subnets)[*].id
   private_dns_enabled = true
 }
