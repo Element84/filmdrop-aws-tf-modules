3c0c96f85b473655e449b8cf04a21f6cc3f51675
Phil Varner <pvarner@element84.com>
2024-05-29 15:20:41 -0400

    update default stac-server to 3.8.0 and opensearch to 2.13

--- modules/stac-server/inputs.tf
+++ modules/stac-server/inputs.tf
@@ -115,7 +115,7 @@ variable "api_rest_type" {
 variable "opensearch_version" {
   description = "OpenSearch version for OpenSearch Domain"
   type        = string
-  default     = "OpenSearch_2.11"
+  default     = "OpenSearch_2.13"
 }
 
 variable "opensearch_cluster_instance_type" {
