bbc1e843f065c654c0894acb9a2a373a7209babf
Hector Machin <hector@element84.com>
2024-09-12 17:38:11 -0400

    Formatting changes

--- modules/cloudfront/waf/data.tf
+++ modules/cloudfront/waf/data.tf
@@ -3,5 +3,5 @@ data "aws_caller_identity" "current" {
 }
 
 locals {
-  origin_appendix = lower(substr(replace(replace("fd-${var.project_name}-${var.environment}", "-", ""),"_", ""), 0, 18))
+  origin_appendix = lower(substr(replace(replace("fd-${var.project_name}-${var.environment}", "-", ""), "_", ""), 0, 18))
 }
