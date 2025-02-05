587504f8e87390f2a25bc20a4a4923bbb77118fa
Caleb VanGerpen <cvangerpen@element84.com>
2025-02-05 15:50:27 -0600

    Remove template_variables from cirrus workflows (#92)
    This commit reduces the cirrus workflow module's complexity by relying
    on the user to simply provide Cirrus task output attribute lookups
    directly in their workflow state machine definition rather than using
    arbitrary variable names with associated entries under a workflow config
    'template_variables' attribute.
    
    The rendered JSON is now parsed via regexes to determine which resource
    ARNs the workflow IAM role needs permissions to execute. This also
    removes the need for the optional 'non_cirrus_lambda_arns' workflow
    config setting.
    
    The workflow IAM role's lambda and batch policy documents were merged
    into one with conditional statements based on whether lambda or batch
    resources are used by the workflow's tasks.

--- modules/cirrus/inputs.tf
+++ modules/cirrus/inputs.tf
@@ -541,53 +541,24 @@ variable "cirrus_workflows" {
       - name: (required, string) Identifier for the Cirrus Workflow. Must be
         unique across all Cirrus Workflows. Valid characters are: [A-Za-z0-9-]
 
-      - non_cirrus_lambda_arns: (optional, list[string]) List of Lambda function
-        ARNs that'll be executed by the Workflow but are not managed by a Cirrus
-        task. This is necessary for granting the Workflow execution role invoke
-        permissions on these functions.
-
-      - template_filepath: (required, string) Path to an Amazon State Machine
-        definition template file. The path must be relative to the ROOT module
-        of the Terraform deployment. The template should use valid Amazon States
-        Language syntax; wherever a Cirrus Task resource ARN is needed, a
+      - state_machine_filepath: (required, string) Path to an Amazon State
+        Machine definition template file. The path must be relative to the ROOT
+        module of the Terraform deployment. The template should use valid Amazon
+        States Language syntax; wherever a Cirrus Task resource ARN is needed, a
         Terraform interpolation sequence (a "$\{...}" without the "\") may be
-        used instead. The variable name does not matter so long as there is a
-        corresponding entry in the "template_variables" argument.
+        used instead. The interpolation sequence should have the following form:
+          <TASK NAME>.<TASK TYPE>.<TASK ATTR>
+
+        Where:
+          <TASK NAME> : name of the task
+          <TASK TYPE> : one of [lambda, batch]
+          <TASK ATTR> : one of [function_arn, job_definition_arn, job_queue_arn]
+
         Example template snippet:
-
           "States": {
             "FirstState": {
               "Type": "Task",
-              "Resource": "$\{my-task-lambda}",  // REMOVE THE "\"
-              "Next": "SecondState",
-              ...
-            },
-
-        Cirrus may deploy and manage several builtin tasks. Resource ARNs for
-        these tasks may be referenced in a Workflow template using a predefined
-        variable name without having to supply a 'template_variable' entry.
-          - If Batch Tasks were created, the following variables may be used:
-            - PRE-BATCH: cirrus-geo pre-batch Lambda function ARN
-            - POST-BATCH: cirrus-geo post-batch Lambda function ARN
-
-      - template_variables: (optional, map[object]) A map of template variable
-        names to their corresponding Cirrus Task attributes. Assuming a Cirrus
-        Task named "my-task" with Lambda config was passed to the 'task' module,
-        the following workflow template variable config:
-
-          my-task-lambda = {
-            task_name = "my-task"
-            task_type = "lambda"
-            task_attr = "functon_arn"
-          }
-
-        when used with the example Workflow snippet above would result in the
-        following content after template interpolation:
-
-          "States": {
-            "FirstState": {
-              "Type": "Task",
-              "Resource": "arn:aws:lambda:us-west-2:123456789012:function:my-function",
+              "Resource": "$\{my-task.lambda.function_arn}",  // REMOVE THE "\"
               "Next": "SecondState",
               ...
             },
@@ -595,13 +566,7 @@ variable "cirrus_workflows" {
 
   type = list(object({
     name                   = string
-    non_cirrus_lambda_arns = optional(list(string))
-    template_filepath      = string
-    template_variables = optional(map(object({
-      task_name = string
-      task_type = string
-      task_attr = string
-    })))
+    state_machine_filepath = string
   }))
 
   default  = []
