3d388bfdf99253ad1dd2ab433e3004f6dd6eb1c2
Theodore Reuter <theodore.reuter@gmail.com>
2025-03-05 16:51:48 -0500

    feat: reworking parameter store params and name construction and correctly building base workflow ARN

--- modules/cirrus/param_store.tf
+++ modules/cirrus/param_store.tf
@@ -1,68 +1,69 @@
 locals {
-  pointer_prefix    = "/cirrus/deployments/"
-  deployment_name   = "${var.project_name}-${var.environment}"
-  deployment_prefix = "/deployment/"
+  pointer_prefix   = "/cirrus/deployments/"
+  deployment_name  = "${var.project_name}-${var.environment}"
+  parameter_prefix = "/deployment/${local.deployment_name}/"
 }
+
 resource "aws_ssm_parameter" "deployment_pointer" {
   name = "${local.pointer_prefix}${local.deployment_name}"
   type = "String"
   value = jsonencode({
     type  = "parameter_store"
-    value = "${local.deployment_prefix}${local.deployment_name}"
+    value = local.parameter_prefix
     }
   )
 }
 
 resource "aws_ssm_parameter" "event_db_and_table" {
-  name  = "${local.deployment_prefix}${local.deployment_name}/CIRRUS_EVENT_DB_AND_TABLE"
+  name  = "${local.parameter_prefix}CIRRUS_EVENT_DB_AND_TABLE"
   type  = "String"
   value = "${module.base.cirrus_state_event_timestreamwrite_database_name}|${module.base.cirrus_state_event_timestreamwrite_table_name}"
 }
 
 resource "aws_ssm_parameter" "payload_bucket" {
-  name  = "${local.deployment_prefix}${local.deployment_name}/CIRRUS_PAYLOAD_BUCKET"
+  name  = "${local.parameter_prefix}CIRRUS_PAYLOAD_BUCKET"
   type  = "String"
   value = module.base.cirrus_payload_bucket
 }
 
 resource "aws_ssm_parameter" "publish_topic_arn" {
-  name  = "${local.deployment_prefix}${local.deployment_name}/CIRRUS_PUBLISH_TOPIC_ARN"
+  name  = "${local.parameter_prefix}CIRRUS_PUBLISH_TOPIC_ARN"
   type  = "String"
   value = module.base.cirrus_publish_sns_topic_arn
 }
 
 resource "aws_ssm_parameter" "process_queue_url" {
-  name  = "${local.deployment_prefix}${local.deployment_name}/CIRRUS_PROCESS_QUEUE_URL"
+  name  = "${local.parameter_prefix}CIRRUS_PROCESS_QUEUE_URL"
   type  = "String"
   value = module.base.cirrus_process_sqs_queue_url
 }
 
 resource "aws_ssm_parameter" "workflow_event_topic_arn" {
-  name  = "${local.deployment_prefix}${local.deployment_name}/CIRRUS_WORKFLOW_EVENT_TOPIC_ARN"
+  name  = "${local.parameter_prefix}CIRRUS_WORKFLOW_EVENT_TOPIC_ARN"
   type  = "String"
   value = module.base.cirrus_workflow_event_sns_topic_arn
 }
 
 resource "aws_ssm_parameter" "log_level" {
-  name  = "${local.deployment_prefix}${local.deployment_name}/CIRRUS_LOG_LEVEL"
+  name  = "${local.parameter_prefix}CIRRUS_LOG_LEVEL"
   type  = "String"
   value = "DEBUG"
 }
 
 resource "aws_ssm_parameter" "state_db" {
-  name  = "${local.deployment_prefix}${local.deployment_name}/CIRRUS_STATE_DB"
+  name  = "${local.parameter_prefix}CIRRUS_STATE_DB"
   type  = "String"
   value = module.base.cirrus_state_dynamodb_table_name
 }
 
 resource "aws_ssm_parameter" "base_workflow_arn" {
-  name  = "${local.deployment_prefix}${local.deployment_name}/CIRRUS_BASE_WORKFLOW_ARN"
+  name  = "${local.parameter_prefix}CIRRUS_BASE_WORKFLOW_ARN"
   type  = "String"
-  value = aws_iam_role.workflow_machine.arn
+  value = "arn:aws:states:${local.current_region}:${local.current_account}:stateMachine:${local.cirrus_prefix}"
 }
 
 resource "aws_ssm_parameter" "cirrus_prefix" {
-  name  = "${local.deployment_prefix}${local.deployment_name}/CIRRUS_PREFIX"
+  name  = "${local.parameter_prefix}CIRRUS_PREFIX"
   type  = "String"
-  value = module.base.cirrus_prefix
+  value = local.cirrus_prefix
 }
