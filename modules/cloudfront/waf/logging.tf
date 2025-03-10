9636fafd87136a557065bc8bfefe3776ec41e1bc
Hector Machin <hector@element84.com>
2025-03-10 12:21:18 -0400

    CI fixes

--- modules/cloudfront/waf/logging.tf
+++ modules/cloudfront/waf/logging.tf
@@ -1,6 +1,6 @@
 resource "aws_wafv2_web_acl_logging_configuration" "wafv2_logging_configuration" {
-  log_destination_configs = [aws_kinesis_firehose_delivery_stream.wafv2_logging_firehose_stream.arn]
-  resource_arn            = aws_wafv2_web_acl.cf_web_acl.arn
+  log_destination_configs = [aws_kinesis_firehose_delivery_stream.fd_waf_cf_logging_firehose_stream.arn]
+  resource_arn            = aws_wafv2_web_acl.fd_waf_acl.arn
 }
 
 resource "aws_kinesis_firehose_delivery_stream" "fd_waf_cf_logging_firehose_stream" {
