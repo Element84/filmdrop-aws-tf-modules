fd757c0464a9902aa0afb30fe6d0e0a35363b080
Hector Machin <38859646+hectormachin@users.noreply.github.com>
2025-05-15 12:15:41 -0400

    Fixes for external content bucket support for Console UI and Cirrus Dashboard (#146)

--- profiles/cirrus-dashboard/outputs.tf
+++ profiles/cirrus-dashboard/outputs.tf
@@ -1,7 +1,7 @@
 output "cirrus_dashboard_url" {
-  value = "https://${var.cirrus_dashboard_inputs.deploy_cloudfront ? module.cloudfront_s3_website[0].domain_name : var.cirrus_dashboard_inputs.deploy_s3_bucket == false ? var.cirrus_dashboard_inputs.external_content_bucket.external_content_bucket_regional_domain_name : module.content_website[0].external_content_bucket_regional_domain_name}"
+  value = "https://${var.cirrus_dashboard_inputs.deploy_cloudfront ? module.cloudfront_s3_website[0].domain_name : var.cirrus_dashboard_inputs.deploy_s3_bucket == false ? var.cirrus_dashboard_inputs.external_content_bucket.external_content_bucket_regional_domain_name : module.content_website[0].content_bucket_regional_domain_name}"
 }
 
 output "cirrus_dashboard_bucket_name" {
-  value = var.cirrus_dashboard_inputs.deploy_cloudfront ? module.cloudfront_s3_website[0].content_bucket_name : module.content_website[0].content_bucket
+  value = var.cirrus_dashboard_inputs.deploy_s3_bucket == false ? var.cirrus_dashboard_inputs.external_content_bucket.external_content_website_bucket_name : var.cirrus_dashboard_inputs.deploy_cloudfront ? module.cloudfront_s3_website[0].content_bucket_name : module.content_website[0].content_bucket
 }
