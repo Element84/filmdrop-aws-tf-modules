7aeb1b69255b3bd045afddb8f72657a92d65f9d0
Hector Machin <hector@element84.com>
2025-05-16 15:38:51 -0400

    Adding capability for private domain name for API Gateway for Stac and Cirrus

--- modules/mosaic-titiler/titler-mosaicjson-private-api.tf
+++ modules/mosaic-titiler/titler-mosaicjson-private-api.tf
@@ -322,7 +322,7 @@ resource "aws_api_gateway_domain_name_access_association" "titiler_api_gateway_d
 
 resource "aws_api_gateway_base_path_mapping" "titiler_api_gateway_domain_mapping" {
   count       = var.is_private_endpoint == true && var.domain_alias != "" && var.private_certificate_arn != "" ? 1 : 0
-  domain_name = var.domain_alias
+  domain_name = aws_api_gateway_domain_name.titiler_api_gateway_domain_name[0].id
   api_id      = aws_api_gateway_rest_api.titiler_api_gateway[0].id
-  stage_name  = var.environment
+  stage_name  = aws_api_gateway_deployment.titiler_api_gateway[0].stage_name
 }
