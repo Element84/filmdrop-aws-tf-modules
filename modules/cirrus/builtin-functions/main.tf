350be14f719d68f3ee261bbb038f80d5c2d0b6e0
Nga Chung <nchung@element84.com>
2025-09-05 10:50:44 -0700

    fix: added --fail to curl command

--- modules/cirrus/builtin-functions/main.tf
+++ modules/cirrus/builtin-functions/main.tf
@@ -7,6 +7,6 @@ resource "null_resource" "get_cirrus_lambda" {
     always_run = var.cirrus_lambda_version
   }
   provisioner "local-exec" {
-    command = "curl -s -L -o ${local.cirrus_lambda_filename} https://github.com/cirrus-geo/cirrus-geo/releases/download/v${var.cirrus_lambda_version}/cirrus-lambda-dist.zip"
+    command = "curl -s -L -o ${local.cirrus_lambda_filename} --fail https://github.com/cirrus-geo/cirrus-geo/releases/download/v${var.cirrus_lambda_version}/cirrus-lambda-dist.zip"
   }
 }
