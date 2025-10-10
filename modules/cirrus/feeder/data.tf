b08c065ad8cd0464542a395dbb422abc5f21d393
John Aitchison <12837598+KayakinCoder@users.noreply.github.com>
2025-10-10 00:14:50 +0000

    lintfix

--- modules/cirrus/feeder/data.tf
+++ modules/cirrus/feeder/data.tf
@@ -1,10 +1,3 @@
-data "aws_caller_identity" "current" {}
-data "aws_region" "current" {}
-
 locals {
   name_main = lower("${var.resource_prefix}-feeder-${var.feeder_config.name}")
-
-  # Save as locals to avoid obnoxiously long lines
-  current_account = data.aws_caller_identity.current.account_id
-  current_region  = data.aws_region.current.name
 }
--- modules/cirrus/feeder/lambda/data.tf
+++ modules/cirrus/feeder/lambda/data.tf
@@ -1,10 +1,8 @@
 data "aws_caller_identity" "current" {}
-data "aws_region" "current" {}
 
 locals {
   # Save as locals to avoid obnoxiously long lines
   current_account = data.aws_caller_identity.current.account_id
-  current_region  = data.aws_region.current.name
 
   # For determining whether a task image is in ECR.
   # Used in conjunction with `resolve_ecr_tag_to_digest`.
