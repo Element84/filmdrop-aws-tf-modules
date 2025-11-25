45fd8ff6817e93ed67a47049448b6762755f3176
Ian Cooke <icooke@element84.com>
2025-11-25 14:33:13 -0500

    pr-feedback: correct metrics module output refs

--- modules/cirrus/builtin_functions.tf
+++ modules/cirrus/builtin_functions.tf
@@ -40,11 +40,11 @@ module "builtin_functions" {
   cirrus_process_sqs_queue_url                     = module.base.cirrus_process_sqs_queue_url
   cirrus_update_state_dead_letter_sqs_queue_arn    = module.base.cirrus_update_state_dead_letter_sqs_queue_arn
   cirrus_workflow_metrics_enabled                  = var.cirrus_workflow_metrics_enabled
-  cirrus_workflow_metrics_log_group_name           = var.cirrus_workflow_metrics_enabled ? module.metrics.cirrus_workflow_metrics_log_group_name : ""
-  cirrus_workflow_metrics_namespace                = var.cirrus_workflow_metrics_enabled ? module.metrics.cirrus_workflow_metrics_namespace : ""
-  cirrus_workflow_metrics_write_policy_arn         = var.cirrus_workflow_metrics_enabled ? module.metrics.cirrus_workflow_metrics_write_policy_arn : ""
-  cirrus_workflow_metrics_read_policy_arn          = var.cirrus_workflow_metrics_enabled ? module.metrics.cirrus_workflow_metrics_read_policy_arn : ""
-  warning_sns_topic_arn                            = var.warning_sns_topic_arn
+  cirrus_workflow_metrics_log_group_name           = var.cirrus_workflow_metrics_enabled ? module.metrics[0].cirrus_workflow_metrics_log_group_name : ""
+  cirrus_workflow_metrics_namespace                = var.cirrus_workflow_metrics_enabled ? module.metrics[0].cirrus_workflow_metrics_namespace : ""
+  cirrus_workflow_metrics_write_policy_arn         = var.cirrus_workflow_metrics_enabled ? module.metrics[0].cirrus_workflow_metrics_write_policy_arn : ""
+  cirrus_workflow_metrics_read_policy_arn          = var.cirrus_workflow_metrics_enabled ? module.metrics[0].cirrus_workflow_metrics_read_policy_arn : ""
+  warningb_sns_topic_arn                           = var.warning_sns_topic_arn
   critical_sns_topic_arn                           = var.critical_sns_topic_arn
   deploy_alarms                                    = var.deploy_alarms
   domain_alias                                     = var.domain_alias
