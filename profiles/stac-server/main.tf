89ff98b20f7190ceb575936ef01c94941aa0e279
John Aitchison <12837598+KayakinCoder@users.noreply.github.com>
2026-01-03 16:33:10 +0000

    init update

--- profiles/stac-server/main.tf
+++ profiles/stac-server/main.tf
@@ -1,7 +1,5 @@
 module "stac-server" {
-  #TODO: update to v2.0.0 before merging, and delete lint ignore
-  # tflint-ignore: terraform_module_pinned_source
-  source = "git::https://github.com/Element84/terraform-aws-stac-server.git?ref=main"
+  source = "git::https://github.com/Element84/terraform-aws-stac-server.git?ref=v2.0.0"
 
   deploy_local_stac_server_artifacts          = var.deploy_local_stac_server_artifacts
   stac_server_version                         = var.stac_server_inputs.version
